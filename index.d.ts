b7e52f2f03763a13ca53d8af9c61aae9bde2f792
Zim <zimtsui@gmail.com>
2020-08-25 16:58:32 +0800

    added .d.ts

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import { Server } from 'http';
+
+declare interface Callback {
+    (err?: Error): void;
+}
+
+declare interface DestroyableServer extends Server {
+    destroy(cb: Callback): void;
+}
+
+declare function enableDestroy(server: Server): DestroyableServer;
+
+export {
+    enableDestroy as default,
+    enableDestroy,
+    DestroyableServer,
+    Callback,
+}
